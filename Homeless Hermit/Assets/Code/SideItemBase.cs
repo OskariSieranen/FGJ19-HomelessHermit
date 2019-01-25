5a700177ee573dab1ce2b6ce846c76d5a42d2334
Leevi Pietiläinen <leevi.pietilainen@outlook.com>
2019-01-26 01:40:50 +0200

    Added simple item that gets eaten when hit.

---
+++ Homeless Hermit/Assets/Code/SideItemBase.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class SideItemBase : MonoBehaviour
+{
+    public SpriteRenderer sprite;
+    public BoxCollider2D boxCollider;
+}
--- Homeless Hermit/Assets/Code/SidePlayer.cs
+++ Homeless Hermit/Assets/Code/SidePlayer.cs
@@ -19,17 +19,37 @@ public class SidePlayer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKey(KeyCode.LeftArrow))
+        if (Input.GetKey(KeyCode.LeftArrow))
         {
-            body.velocity = new Vector3(-movementForce, body.velocity.y);
+            body.velocity = new Vector2(-movementForce, body.velocity.y);
         }
         if (Input.GetKey(KeyCode.RightArrow))
         {
-            body.velocity = new Vector3(movementForce, body.velocity.y);
+            body.velocity = new Vector2(movementForce, body.velocity.y);
         }
         if (Input.GetKeyDown(KeyCode.UpArrow) && IsGrounded())
         {
             body.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
+
+        PreventOutOfCamera();
+    }
+
+    void PreventOutOfCamera()
+    {
+        Vector3 pos = Camera.main.WorldToViewportPoint(transform.position);
+        if(pos.x < 0.01f)
+        {
+            body.velocity = new Vector2(Mathf.Abs(body.velocity.x), body.velocity.y);
+            //transform.position = new Vector3(0.01f, transform.position.y);
+        }
+        else if (1.0 < pos.x)
+        {
+            body.velocity = new Vector2(-Mathf.Abs(body.velocity.x), body.velocity.y);
+        }
+        else if (pos.y < 0.0)
+        {
+            //We're below the camera, initiate rest
         }
     }
 
@@ -37,5 +57,15 @@ public class SidePlayer : MonoBehaviour
     {
         RaycastHit2D hit = Physics2D.Raycast(rayOrigin.transform.position, Vector2.down, rayCheckDistance);
         return hit.collider != null;
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        var itemCollision = col.gameObject.GetComponent<SideItemBase>();
+        if (itemCollision != null)
+        {
+            //TODO: Put item to inventory
+            Destroy(col.gameObject);
+        }
     }
 }
