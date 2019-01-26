bb3aea6d3e4e7629d1cc73660199f45ef7e5c78a
Leevi Pietiläinen <leevi.pietilainen@outlook.com>
2019-01-26 19:37:13 +0200

    Floating effect for items. Added poison skull. Disabled doublejump(bug).  Added fruit to 2nd level.

---
+++ Homeless Hermit/Assets/Code/FloatingItem.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloatingItem : MonoBehaviour
+{
+    Vector2 floatY;
+    float originalY;
+
+    public float floatStrength = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        originalY = transform.position.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(transform.position.x,
+            originalY + ((float)Mathf.Sin(Time.time) * floatStrength),
+            transform.position.z);
+    }
+}
